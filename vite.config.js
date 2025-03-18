78a3ddfa5be9655b0dbfca5cdeefb562057d91e6
kevgenga <kevin.lao@hotmail.fr>
2025-03-18 20:17:17 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/test-portfolio-mangaka/' : '/',  // Base différente en fonction de l'environnement
+  base: '/test-portfolio-mangaka/',  // Ajoute cette ligne
   server: {
     host: true, // Permet l'accès depuis d'autres appareils
     port: 5173, // Assure que le port reste le même
